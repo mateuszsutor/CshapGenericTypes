631f6b39e507d72e7bdba086ac3e35b01461e286
Matys <mateusz.sutor@icloud.com>
2020-08-23 19:25:59 +0200

    Dodanie UnitTestów

---
+++ CshapGenericTypes/GenrricTypeTest/CircleQueueTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using _1_GenericType;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenrricTypeTest
+{
+    [TestClass]
+    public class CircleQueueTests
+    {
+
+        [TestMethod]
+        public void NewQueueIsEmpty()
+        {
+            var queue = new CircleQueue();
+
+            Assert.IsTrue(queue.IsEmpty);
+        }
+
+        [TestMethod]
+        public void ThreeElementsQueueAfterAddThreeElementIsFull()
+        {
+            var queue = new CircleQueue(capacity: 3);
+            queue.SaveValue(3.3);
+            queue.SaveValue(5);
+            queue.SaveValue(10.8);
+
+            Assert.IsTrue(queue.IsFull);
+        }
+
+
+        [TestMethod]
+        public void HasFirstElementAddToQueueIsFirstRead()
+        {
+            var queue = new CircleQueue(capacity: 3);
+
+            var value1 = 8.8;
+            var value2 = 5.1;
+
+            queue.SaveValue(value1);
+            queue.SaveValue(value2);
+
+
+            Assert.AreEqual(value1, queue.ReadValue());
+            Assert.AreEqual(value2, queue.ReadValue());
+            Assert.IsTrue(queue.IsEmpty);
+        }
+
+        [TestMethod]
+        public void HasOverwriteValueWhenCapacityIsExceed()
+        {
+            var queue = new CircleQueue(capacity: 3);
+
+            var values = new[] { 1.8, 3.5, 5.1, 7.6, 9.1, 11.0 };
+
+            foreach (var item in values)
+            {
+                queue.SaveValue(item);
+            }
+
+            Assert.IsTrue(queue.IsFull);
+            Assert.AreEqual(values[3], queue.ReadValue());
+            Assert.AreEqual(values[4], queue.ReadValue());
+            Assert.AreEqual(values[5], queue.ReadValue());
+            Assert.IsTrue(queue.IsEmpty);
+        }
+    }
+}
---
+++ CshapGenericTypes/GenrricTypeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GenrricTypeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenrricTypeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("13ab9fc5-7c41-4ee6-aa92-632305552b17")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

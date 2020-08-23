d5402964ddc85225dc82203a7e746a2410a5baeb
Matys <mateusz.sutor@icloud.com>
2020-08-23 21:18:05 +0200

    Mała refaktoryzajca

--- CshapGenericTypes/1_GenericType/Program.cs
+++ CshapGenericTypes/1_GenericType/Program.cs
@@ -6,56 +6,69 @@ namespace _1_GenericType
     {
         static void Main(string[] args)
         {
-            //var queue = new CircleQueue<double>(capacity: 3);
+            var queue = new CircleQueue<double>(capacity: 3);
 
-            var pesonsQueue = new CircleQueue<Person>();
-            pesonsQueue.SaveValue(new Person { FirstName = "Jan", LastName = "Kowalski" });
-            pesonsQueue.SaveValue(new Person { FirstName = "Tomasz", LastName = "Nowak" });
-            pesonsQueue.SaveValue(new Person { FirstName = "Ewa", LastName = "Zawadzka" });
-            pesonsQueue.SaveValue(new Person { FirstName = "Ala", LastName = "Kot" });
+            InputData(queue);
+            ProcessingData(queue);
 
+            Console.ReadKey();
+        }
 
-            while (!pesonsQueue.IsEmpty)
+        private static void ProcessingData(CircleQueue<double> queue)
+        {
+            var sum = 0.0;
+            Console.WriteLine("W naszej kolejce znajdują się: ");
+
+            while (!queue.IsEmpty)
             {
 
-                var person = pesonsQueue.ReadValue().FirstName;
-                Console.WriteLine(person);
+                sum += queue.ReadValue();
+
             }
 
-            //while (true)
+            #region personQueue
+            //var pesonsQueue = new CircleQueue<Person>();
+            //pesonsQueue.SaveValue(new Person { FirstName = "Jan", LastName = "Kowalski" });
+            //pesonsQueue.SaveValue(new Person { FirstName = "Tomasz", LastName = "Nowak" });
+            //pesonsQueue.SaveValue(new Person { FirstName = "Ewa", LastName = "Zawadzka" });
+            //pesonsQueue.SaveValue(new Person { FirstName = "Ala", LastName = "Kot" });
+
+
+            //while (!pesonsQueue.IsEmpty)
             //{
 
-            //    var value = 0.0;
-            //    var startValue = Console.ReadLine();
-
-            //    if (double.TryParse(startValue, out value))
-            //    {
-            //        queue.SaveValue(value);
-            //        continue;
-            //    }
-            //    break;
-
+            //    var person = pesonsQueue.ReadValue().FirstName;
+            //    Console.WriteLine(person);
             //}
 
-            //var sum = 0.0;
-            //Console.WriteLine("W naszej kolejce znajdują się: ");
+            #endregion
 
-            //while (!queue.IsEmpty)
-            //{
+            Console.WriteLine(sum);
+        }
 
-            //    sum += queue.ReadValue();
+        private static void InputData(CircleQueue<double> queue)
+        {
+            while (true)
+            {
 
-            //}
+                var value = 0.0;
+                var startValue = Console.ReadLine();
 
-            //Console.WriteLine(sum);
-            Console.ReadKey();
+                if (double.TryParse(startValue, out value))
+                {
+                    queue.SaveValue(value);
+                    continue;
+                }
+                break;
+            }
         }
     }
 
-    public class Person
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-
+    #region class Person
+    //public class Person
+    //{
+    //    public string FirstName { get; set; }
+    //    public string LastName { get; set; }
+    //}
+    #endregion
 }

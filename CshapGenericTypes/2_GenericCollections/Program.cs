94fbe52b4289141cb3922c9dba493f53ebfa7205
Matys <mateusz.sutor@icloud.com>
2020-08-25 22:17:23 +0200

    Dodanie dodanie testów jednostkowych QueueTests

--- CshapGenericTypes/2_GenericCollections/Program.cs
+++ CshapGenericTypes/2_GenericCollections/Program.cs
@@ -35,25 +35,50 @@ namespace _2_GenericCollections
             //Console.WriteLine(employees.Length);
             #endregion
 
+            #region List<T> info
+            //List<Employee> employessList = new List<Employee>
+
+            //{
+            //    new Employee { FirstName = "Jan", LastName = "Kowalski" },
+            //    new Employee { FirstName = "Tomasz", LastName = "Nowak" },
+            //    new Employee { FirstName = "Ela", LastName = "Zawadzka" },
+            //};
+
+            //foreach (var employee in employees)
+
+            //    employessList.Add(new Employee { FirstName = "Ania", LastName = "Kot" });
+
+            //foreach (var employee in employessList)
+            //{
+            //    Console.WriteLine(employee.FirstName + " " + employee.LastName);
+            //}
+
+            //for (int i = 0; i < employees.Length; i++)
+            //    for (int i = 0; i < employessList.Count; i++)
+            //    {
+            //        Console.WriteLine(employees[i].FirstName + " - " + employees[i].LastName);
+            //        Console.WriteLine(employessList[i].FirstName + " - " + employessList[i].LastName);
+            //    }
+            #endregion
 
             #region Queue<T> info
 
-            Queue<Employee> employeesQueue = new Queue<Employee>();
+            //Queue<Employee> employeesQueue = new Queue<Employee>();
 
-            //Dodawanie zawsze na koniec kolejki
-            employeesQueue.Enqueue(new Employee { FirstName = "Jan", LastName = "Kowalski" });
-            employeesQueue.Enqueue(new Employee { FirstName = "Tomasz", LastName = "Nowak" });
-            employeesQueue.Enqueue(new Employee { FirstName = "Henryk", LastName = "Kania" });
-            employeesQueue.Enqueue(new Employee { FirstName = "Ewa", LastName = "Zawadzka" });
+            ////Dodawanie zawsze na koniec kolejki
+            //employeesQueue.Enqueue(new Employee { FirstName = "Jan", LastName = "Kowalski" });
+            //employeesQueue.Enqueue(new Employee { FirstName = "Tomasz", LastName = "Nowak" });
+            //employeesQueue.Enqueue(new Employee { FirstName = "Henryk", LastName = "Kania" });
+            //employeesQueue.Enqueue(new Employee { FirstName = "Ewa", LastName = "Zawadzka" });
 
 
-            while (employeesQueue.Count > 0)
-            {
+            //while (employeesQueue.Count > 0)
+            //{
 
-                Console.WriteLine(employeesQueue.Dequeue().ToString());
-            }
+            //    Console.WriteLine(employeesQueue.Dequeue().ToString());
+            //}
 
-            Console.WriteLine("Kojejka została wyczyszczona");
+            //Console.WriteLine("Kojejka została wyczyszczona");
 
             #endregion
         }
---
+++ CshapGenericTypes/GenericCollectionsTests/QueueTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCollectionsTests
+{
+    [TestClass]
+    public class QueueTest
+    {
+        [TestMethod]
+        public void UsePeekMethod() 
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+
+            //Peek() tylko podgląda co jest na pierwszym miejscu w kolejce !!! Nie usuwa elementu!!!
+            Assert.AreEqual(1, queue.Peek());
+        }
+
+
+        [TestMethod]
+        public void UseContainsMethod()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            
+            Assert.IsTrue(queue.Contains(3));
+            Assert.IsFalse(queue.Contains(9));
+
+        }
+
+
+        [TestMethod]
+        public void UseToArrayMethod()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            var array = queue.ToArray();
+            queue.Dequeue();
+
+            Assert.AreEqual(1, array.First());
+            Assert.AreEqual(3, queue.Count());
+
+        }
+
+
+        [TestMethod]
+        public void UseClearMethod()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            queue.Clear();
+
+            Assert.AreEqual(0, queue.Count());
+
+        }
+
+
+    }
+}

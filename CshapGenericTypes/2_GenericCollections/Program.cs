f10b76d6e3dd02cbf7d6f5646298159bd99c95fc
Matys <mateusz.sutor@icloud.com>
2020-08-26 20:49:54 +0200

    Refaktoring

--- CshapGenericTypes/2_GenericCollections/Program.cs
+++ CshapGenericTypes/2_GenericCollections/Program.cs
@@ -8,82 +8,70 @@ namespace _2_GenericCollections
         static void Main(string[] args)
         {
 
-            #region Array info
 
-            //Employee[] employees = new Employee[]
-            //{
-            //    new Employee { FirstName = "Jan", LastName = "Kowalski" },
-            //    new Employee { FirstName = "Tomasz", LastName = "Nowak" },
-            //    new Employee { FirstName = "Ela", LastName = "Zawadzka" },
-            //};
+            //Array();
+            //List();
+            //Queue();
+            //Stack();
 
-            //foreach (var employee in employees)
-            //{
-            //    Console.WriteLine(employee.FirstName + " " + employee.LastName);
-            //}
+        }
 
-            //for (int i = 0; i < employees.Length; i++)
-            //{
-            //    Console.WriteLine(employees[i].FirstName + " - " + employees[i].LastName);
-            //}
-
-            //// resize old array lenght
-            //Array.Resize(ref employees, 10);
-
-            //employees[8] = new Employee { FirstName = "Ala", LastName = "Kot" };
-
-            //Console.WriteLine(employees.Length);
-            #endregion
-
-            #region List<T> info
-            //List<Employee> employessList = new List<Employee>
-
-            //{
-            //    new Employee { FirstName = "Jan", LastName = "Kowalski" },
-            //    new Employee { FirstName = "Tomasz", LastName = "Nowak" },
-            //    new Employee { FirstName = "Ela", LastName = "Zawadzka" },
-            //};
-
-            //foreach (var employee in employees)
-
-            //    employessList.Add(new Employee { FirstName = "Ania", LastName = "Kot" });
-
-            //foreach (var employee in employessList)
-            //{
-            //    Console.WriteLine(employee.FirstName + " " + employee.LastName);
-            //}
-
-            //for (int i = 0; i < employees.Length; i++)
-            //    for (int i = 0; i < employessList.Count; i++)
-            //    {
-            //        Console.WriteLine(employees[i].FirstName + " - " + employees[i].LastName);
-            //        Console.WriteLine(employessList[i].FirstName + " - " + employessList[i].LastName);
-            //    }
-            #endregion
-
-            #region Queue<T> info
-            //First In Fist Out  - FIFO
-            Queue<Employee> employeesQueue = new Queue<Employee>();
-
-            //Dodawanie zawsze na koniec kolejki
-            employeesQueue.Enqueue(new Employee { FirstName = "Jan", LastName = "Kowalski" });
-            employeesQueue.Enqueue(new Employee { FirstName = "Tomasz", LastName = "Nowak" });
-            employeesQueue.Enqueue(new Employee { FirstName = "Henryk", LastName = "Kania" });
-            employeesQueue.Enqueue(new Employee { FirstName = "Ewa", LastName = "Zawadzka" });
-           
-            Console.WriteLine("Kolejka");
-
-            while (employeesQueue.Count > 0)
+        private static void Array()
+        {
+            Employee[] employees = new Employee[]
             {
+                new Employee { FirstName = "Jan", LastName = "Kowalski" },
+                new Employee { FirstName = "Tomasz", LastName = "Nowak" },
+                new Employee { FirstName = "Ela", LastName = "Zawadzka" },
+            };
 
-                Console.WriteLine(employeesQueue.Dequeue().ToString());
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.LastName);
             }
 
-            Console.WriteLine("Kojejka została wyczyszczona");
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine(employees[i].FirstName + " - " + employees[i].LastName);
+            }
 
-            #endregion
+            // resize old array lenght
+            System.Array.Resize(ref employees, 10);
 
-            #region Stack<T> info
+            employees[8] = new Employee { FirstName = "Ala", LastName = "Kot" };
+
+            Console.WriteLine(employees.Length);
+        }
+
+        private static void List()
+        {
+            List<Employee> employessList = new List<Employee>
+
+            {
+                new Employee { FirstName = "Jan", LastName = "Kowalski" },
+                new Employee { FirstName = "Tomasz", LastName = "Nowak" },
+                new Employee { FirstName = "Ela", LastName = "Zawadzka" },
+            };
+
+            foreach (var employee in employees)
+
+                employessList.Add(new Employee { FirstName = "Ania", LastName = "Kot" });
+
+            foreach (var employee in employessList)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.LastName);
+            }
+
+            for (int i = 0; i < employees.Length; i++)
+                for (int i = 0; i < employessList.Count; i++)
+                {
+                    Console.WriteLine(employees[i].FirstName + " - " + employees[i].LastName);
+                    Console.WriteLine(employessList[i].FirstName + " - " + employessList[i].LastName);
+                }
+        }
+
+        private static void Stack()
+        {
             //Last In Last Out - LIFO
             Stack<Employee> employeesStack = new Stack<Employee>();
 
@@ -103,8 +91,28 @@ namespace _2_GenericCollections
             }
 
             Console.WriteLine("Stos został wyczyszczony");
+        }
 
-            #endregion
+        private static void Queue()
+        {
+            //First In Fist Out  - FIFO
+            Queue<Employee> employeesQueue = new Queue<Employee>();
+
+            //Dodawanie zawsze na koniec kolejki
+            employeesQueue.Enqueue(new Employee { FirstName = "Jan", LastName = "Kowalski" });
+            employeesQueue.Enqueue(new Employee { FirstName = "Tomasz", LastName = "Nowak" });
+            employeesQueue.Enqueue(new Employee { FirstName = "Henryk", LastName = "Kania" });
+            employeesQueue.Enqueue(new Employee { FirstName = "Ewa", LastName = "Zawadzka" });
+
+            Console.WriteLine("Kolejka");
+
+            while (employeesQueue.Count > 0)
+            {
+
+                Console.WriteLine(employeesQueue.Dequeue().ToString());
+            }
+
+            Console.WriteLine("Kojejka została wyczyszczona");
         }
     }
 }

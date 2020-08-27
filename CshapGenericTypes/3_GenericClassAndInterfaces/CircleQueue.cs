000e6386fc1a54c99a67aacec8a1563155b2df0a
Matys <mateusz.sutor@icloud.com>
2020-08-27 22:04:30 +0200

    klasy i intefejsy generyczne - start

---
+++ CshapGenericTypes/3_GenericClassAndInterfaces/CircleQueue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_GenericClassAndInterfaces
+{
+    class CircleQueue<T>
+    {
+        private T[] buffer;
+        private int startBuffer;
+        private int endBuffer;
+
+        public CircleQueue() : this(capacity: 5)
+        {
+
+        }
+
+        public CircleQueue(int capacity)
+        {
+            buffer = new T[capacity + 1];
+            startBuffer = 0;
+            endBuffer = 0;
+        }
+
+        public void SaveValue(T value)
+        {
+            buffer[endBuffer] = value;
+            endBuffer = (endBuffer + 1) % buffer.Length;
+
+            if (endBuffer == startBuffer)
+            {
+                startBuffer = (startBuffer + 1) % buffer.Length;
+            }
+
+        }
+
+        public T ReadValue()
+        {
+            var result = buffer[startBuffer];
+            startBuffer = (startBuffer + 1) % buffer.Length;
+
+            return result;
+        }
+
+        public int BufferCapacity
+        {
+            get
+            {
+                return buffer.Length;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return endBuffer == startBuffer;
+            }
+        }
+
+        public bool IsFull
+        {
+            get
+            {
+                return (endBuffer + 1) % buffer.Length == startBuffer;
+            }
+        }
+    }
+}
---
+++ CshapGenericTypes/3_GenericClassAndInterfaces/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_GenericClassAndInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new CircleQueue<double>(capacity: 3);
+
+            InputData(queue);
+            ProcessingData(queue);
+
+            Console.ReadKey();
+        }
+
+        private static void ProcessingData(CircleQueue<double> queue)
+        {
+            var sum = 0.0;
+            Console.WriteLine("W naszej kolejce znajdują się: ");
+
+            while (!queue.IsEmpty)
+            {
+
+                sum += queue.ReadValue();
+
+            }
+
+            #region personQueue
+            //var pesonsQueue = new CircleQueue<Person>();
+            //pesonsQueue.SaveValue(new Person { FirstName = "Jan", LastName = "Kowalski" });
+            //pesonsQueue.SaveValue(new Person { FirstName = "Tomasz", LastName = "Nowak" });
+            //pesonsQueue.SaveValue(new Person { FirstName = "Ewa", LastName = "Zawadzka" });
+            //pesonsQueue.SaveValue(new Person { FirstName = "Ala", LastName = "Kot" });
+
+
+            //while (!pesonsQueue.IsEmpty)
+            //{
+
+            //    var person = pesonsQueue.ReadValue().FirstName;
+            //    Console.WriteLine(person);
+            //}
+
+            #endregion
+
+            Console.WriteLine(sum);
+        }
+
+        private static void InputData(CircleQueue<double> queue)
+        {
+            while (true)
+            {
+
+                var value = 0.0;
+                var startValue = Console.ReadLine();
+
+                if (double.TryParse(startValue, out value))
+                {
+                    queue.SaveValue(value);
+                    continue;
+                }
+                break;
+            }
+
+        }
+    }
+}
